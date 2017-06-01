bc96d80f717dcac3d9e01e2e473d528da67c61bc
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-06-01 07:08:35 -0700

    some refactor to controllers

--- src/com/example/controllers/movie.clj
+++ src/com/example/controllers/movie.clj
@@ -12,38 +12,37 @@
             (response {:error (.getMessage e#)}))))
 
 (defn find-by-id [id t] 
-    (wrap-try 
-        (log/debug "movie find-by-id" id t)
-        (response (dao/find-by-id id t))))
+    (fn [req] 
+        (wrap-try 
+            (log/debug "movie find-by-id" id t)
+            (response (dao/find-by-id id t)))))
 
-(defn create [req] 
-    (wrap-try 
-        (let [body (get-in req [:body :movie])
-              audit (get-in req [:body :audit])]
-        (log/debug "movie create" body)
-        (response {:id (dao/create body audit)}))))
+(defn create [] 
+    (fn [{{movie :movie audit :audit} :body}] 
+        (wrap-try 
+            (log/debug "movie create" movie)
+            (response {:id (dao/create movie audit)}))))
 
-(defn updat [req] 
-    (wrap-try 
-        (let [id (get-in req [:params :id])
-              body (get-in req [:body :movie])
-              audit (get-in req [:body :audit])]
-        (log/debug "movie update id=" id "body=" body)
-        (response (dao/updat id body audit)))))
+(defn updat [id] 
+    (fn [{{movie :movie audit :audit} :body}] 
+        (wrap-try 
+            (log/debug "movie update id=" id "body=" movie)
+            (response (dao/updat id movie audit)))))
 
-(defn delete [req] 
-    (wrap-try
-        (let [id (get-in req [:params :id])
-              audit (get-in req [:body :audit])]
-        (log/debug "movie delete" id)
-        (response (dao/delete id audit)))))
+(defn delete [id] 
+    (fn [{{audit :audit} :body}] 
+        (wrap-try
+            (log/debug "movie delete" id)
+            (response (dao/delete id audit)))))
 
 (defn attribute-history [id root-attr leaf-attr] 
-    (wrap-try
-        (log/debug "movie attribute history" id root-attr leaf-attr)
-        (response {:history (dao/attribute-history id root-attr leaf-attr)})))
+    (fn [req] 
+        (wrap-try
+            (log/debug "movie attribute history" id root-attr leaf-attr)
+            (response {:history (dao/attribute-history id root-attr leaf-attr)}))))
 
 (defn history [id] 
-    (wrap-try
-        (log/debug "movie history" id)
-        (response {:history (dao/history id)})))
+    (fn [req] 
+        (wrap-try
+            (log/debug "movie history" id)
+            (response {:history (dao/history id)}))))
